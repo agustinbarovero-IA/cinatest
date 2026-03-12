580c41e53e4b109e56fcb16ebfd330604be8172a
agustinbarovero-IA <agustinbarovero@gmail.com>
2026-03-12 11:11:10 -0300

    Create estiba.js

---
+++ estiba.js
@@ -0,0 +1,17 @@
+function renderContenedor6Detail(){
+
+screenTitle.textContent="ESTIBA 65984-3";
+
+menuGrid.innerHTML=`
+
+<div class="estiba-detail-wrap">
+
+<button onclick="renderIntervencionForm()">
+Intervenir
+</button>
+
+</div>
+
+`;
+
+}
