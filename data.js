8109a753e11621f24e1f139633c2db36d4f34efb
agustinbarovero-IA <agustinbarovero@gmail.com>
2026-03-12 11:10:26 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const boxesData = [
+
+{
+numero:1,
+titulo:"Contenedor 1",
+ubicacion:"16-5-1-1",
+tipo:"Pallet",
+ocupacion:35,
+estiba:"65981-1"
+},
+
+{
+numero:6,
+titulo:"Contenedor 6",
+ubicacion:"16-5-4-2",
+tipo:"Pallet",
+ocupacion:78,
+estiba:"65984-3"
+}
+
+];
